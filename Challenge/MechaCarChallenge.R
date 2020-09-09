b549e902e15ffdf161f8e7e31e21aadf35a8aba4
viralp353 <65969608+viralp353@users.noreply.github.com>
2020-09-09 18:55:31 -0500

    multiple linear regression

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -1,1 +1,8 @@
-Mecha_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) #import table
+# Import MechrCar mpg files:
+Mecha_table <- read.csv(file='MechaCar_mpg.csv',sep=",", header = T)
+
+#multiple linear regression:
+lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance + AWD,data=Mecha_table) 
+
+#summary:
+summary(lm(mpg ~ vehicle.length + vehicle.weight + spoiler.angle + ground.clearance + AWD,data=Mecha_table))
